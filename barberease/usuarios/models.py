9a549f6d92016e6a6641c040b7327e73fd2bb31b
Ana Pires <ana.pires@palmas.to.gov.br>
2023-10-10 12:47:18 -0300

    feat: :poop: CODIGO COM QUEBRADO!

--- barberease/usuarios/models.py
+++ barberease/usuarios/models.py
@@ -1,24 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from allauth.socialaccount.signals import pre_social_login
-from allauth.socialaccount.models import SocialAccount , SocialLogin
-from django.dispatch import receiver
-from django.shortcuts import redirect
-from django.urls import reverse_lazy
+from usuarios.models import Usuario
+# Create your models here.
 
-
-class Usuario(AbstractUser):
+class Barbearia(models.Model):
+    
     nome = models.CharField("Nome", max_length=150)
-    sobrenome = models.CharField("Sobrenome", max_length=150, null=True, blank=True)
-    email = models.EmailField("Email", unique=True)
-    dono_barbearia= models.BooleanField("Dono da Barbearia", default=False)
+    cnpj = models.CharField("CNPJ", max_length=14, unique=True, default=None)
+    endereco = models.CharField("Endereço", max_length=150)
+    telefone = models.CharField("Telefone", max_length=150)
+    cep = models.CharField("CEP", max_length=9)
+    setor = models.CharField("Setor", max_length=150)
+    cidade = models.CharField("Cidade", max_length=150)
+    estado = models.CharField("Estado", max_length=150)
+    dono = models.ForeignKey(Usuario, verbose_name="Dono", on_delete=models.CASCADE)
     logo = models.ImageField("Logo", upload_to="images", null=True, blank=True)
 
-    first_name = None
-    last_name = None
 
     def __str__(self):
         return self.nome
-    
-
- 
