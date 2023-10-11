5c31cd68a4a072c46b4e36d3bbabd3b7bb9cc24f
Romeu Miranda Borges <romeuborges19@gmail.com>
2023-10-10 21:59:55 -0300

    feat: organizando arquivos para projeto rodar localmente

--- barberease/barberease/settings.py
+++ barberease/barberease/settings.py
@@ -106,11 +106,11 @@ WSGI_APPLICATION = "barberease.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
