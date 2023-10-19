92d61dbf51bcb916433d34f8af813d06956bdada
EnzoKaue <enzo.kaue@mail.uft.edu.br>
2023-10-19 17:45:07 -0300

    mais fix no settings.py

--- barberease/barberease/settings.py
+++ barberease/barberease/settings.py
@@ -14,9 +14,6 @@ from pathlib import Path
 from decouple import config, Csv
 
 import os 
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
