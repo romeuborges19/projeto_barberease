a8f92523b6e3781901d65a653b154ec7d775885d
Ana Pires <ana.pires@palmas.to.gov.br>
2023-10-24 12:39:08 -0300

    feat: :sparkles: implementando back para agenda

--- barberease/agendamento/forms.py
+++ barberease/agendamento/forms.py
@@ -1,28 +1,5 @@
-from django.contrib.admin.options import CheckboxSelectMultiple
-from .models import *
 from django import forms
-
-HORARIOS_DISPONIVEIS = (
-    ("1", "5:00"),
-    ("2", "6:00"),
-    ("3", "7:00"),
-    ("4", "8:00"),
-    ("5", "9:00"),
-    ("6", "10:00"),
-    ("7", "11:00"),
-    ("8", "12:00"),
-    ("9", "13:00"),
-    ("10", "14:00"),
-    ("11", "15:00"),
-    ("12", "16:00"),
-    ("13", "17:00"),
-    ("14", "18:00"),
-    ("15", "19:00"),
-    ("16", "20:00"),
-    ("17", "21:00"),
-    ("18", "22:00"),
-    ("19", "23:00"),
-)
+from .models import Agenda
 
 DIAS_SEMANA = (
     ("segunda", "segunda-feira"),
@@ -34,19 +11,62 @@ DIAS_SEMANA = (
     ("domingo", "domingo"),
 )
 
+HORARIOS = (
+    (1, "07:00"),
+    (2, "08:00"),
+    (3, "09:00"),
+    (4, "10:00"),
+    (5, "11:00"),
+    (6, "12:00"),
+    (7, "13:00"),
+    (8, "14:00"),
+    (9, "15:00"),
+    (10, "16:00"),
+    (11, "17:00"),
+    (12, "18:00"),
+    (13, "19:00"),
+    (14, "20:00"),
+    (15, "21:00"),
+    (16, "22:00"),
+    (17, "23:00"),
+)
+
 class AgendaForm(forms.ModelForm):
-    # Dono preencherá dados que gerarão agenda da barbearia
-
-    def __init__(self, *args, **kwargs):
-        super(AgendaForm, self).__init__(*args, **kwargs)
-
-        for dia, label in DIAS_SEMANA:
-            self.fields[dia] = forms.MultipleChoiceField(
-            choices=HORARIOS_DISPONIVEIS,
-            widget=CheckboxSelectMultiple,
-            required=True,
-            label=label)
 
     class Meta:
         model = Agenda
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(AgendaForm, self).__init__(*args, **kwargs)
+        self.fields['barbearia'].widget = forms.HiddenInput()
+        self.fields['horarios_funcionamento'].widget = forms.HiddenInput()
+        self.fields['dias_semana'].widget = forms.HiddenInput()
+        self.fields['horarios'].widget = forms.HiddenInput()
+
+        for dia in DIAS_SEMANA:
+            self.fields[f'horarios_{dia[0]}'] = forms.MultipleChoiceField(
+                choices=HORARIOS,
+                widget=forms.CheckboxSelectMultiple,
+                required=False,
+            )
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        for dia in DIAS_SEMANA:
+            horarios = cleaned_data.get(f'horarios_{dia[0]}', [])
+            if not horarios:
+                raise forms.ValidationError(f"Selecione pelo menos um horário para {dia[1]}")
+
+        return cleaned_data
+
+
+
+
+
+
+
+
+
+
--- barberease/agendamento/models.py
+++ barberease/agendamento/models.py
@@ -1,14 +1,18 @@
 from django.db import models
-from barbearia.models import Barbearia
+from barbearia.models import Barbearia, Barbeiros
 from usuarios.models import Usuario
 
+MEDIDAS_TEMPO = (
+    (1, "Minutos"),
+    (2, "Horas"),
+)
 class Servico(models.Model):
     # Model que armazena os serviços ofertados por cada barbearia.
 
-    nome = models.CharField("Nome", max_length=50)
-    barbearia = models.ForeignKey(Barbearia, verbose_name="Barbearia", on_delete=models.CASCADE)
-    criado_em = models.DateTimeField(auto_now_add=True)
-    modificado_em = models.DateTimeField(auto_now=True)
+    nome = models.CharField("Nome do Serviço", max_length=50)
+    barbearia = models.OneToOneField(Barbearia, verbose_name="Barbearia", on_delete=models.CASCADE)
+    tempo_servico = models.IntegerField("Tempo 'médio' do serviço")
+    medida_tempo = models.IntegerField("Medida de Tempo", choices=MEDIDAS_TEMPO)
 
     def __str__(self):
         return self.nome
@@ -17,21 +21,19 @@ class Servico(models.Model):
 class Agenda(models.Model):
     # Model que agrupa os agendamentos de uma barbearia e armazena dados sobre sua lógica de funcionamento.
 
-    servicos_hora = models.IntegerField() 
-    horarios_disponiveis = models.JSONField()
-    barbearia = models.ForeignKey(Barbearia, verbose_name="Barbeiro", on_delete=models.CASCADE)
-    criado_em = models.DateTimeField(auto_now_add=True)
-    modificado_em = models.DateTimeField(auto_now=True)
-
-
+    horarios_funcionamento= models.JSONField(null=False, blank=True)
+    barbearia = models.OneToOneField(Barbearia, verbose_name="Barbeiro", on_delete=models.CASCADE, null=False, blank=True)
+    def __str__(self):
+        return self.barbearia.nome
+    
 class Agendamento(models.Model):
     # Model que armazena os dados de um agendamento de serviço de barbearia.
 
     data = models.DateTimeField()
     aprovado = models.BooleanField()
     servico = models.ForeignKey(Servico, verbose_name="Servico", on_delete=models.CASCADE)
-    agenda = models.ForeignKey(Agenda, verbose_name="Agenda", on_delete=models.CASCADE)
+    agenda = models.OneToOneField(Agenda, verbose_name="Agenda", on_delete=models.CASCADE)
     cliente = models.ForeignKey(Usuario, verbose_name="Cliente", related_name="cliente", on_delete=models.CASCADE)
-    barbeiro = models.ForeignKey(Usuario, verbose_name="Barbeiro", related_name="barbeiro", null=True, on_delete=models.SET_NULL)
+    barbeiro = models.ForeignKey(Barbeiros, verbose_name="Barbeiro", related_name="barbeiro", null=True, on_delete=models.SET_NULL)
     criado_em = models.DateTimeField(auto_now_add=True)
     modificado_em = models.DateTimeField(auto_now=True)
