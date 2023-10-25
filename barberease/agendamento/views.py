77fa55267656bac19cc0e627e1d55c966ae05c2f
Romeu Miranda Borges <romeuborges19@gmail.com>
2023-10-25 20:41:32 -0300

    feat: adicionando outra permissão para pagina de criação de agenda

--- barberease/agendamento/views.py
+++ barberease/agendamento/views.py
@@ -4,6 +4,7 @@ from django.views.generic.edit import CreateView
 
 from agendamento.forms import AgendaForm
 from agendamento.models import Agenda
+from barbearia.models import Barbearia
 
 # Create your views here.
 
@@ -14,8 +15,15 @@ class CadastrarAgendaView(CreateView):
 
     def get(self, request, *args, **kwargs):
         if not self.request.session['dono']:
-            print("USUARIO NÃO É DONO")
             return redirect(reverse_lazy("usuario:home"))
+        else:
+            usuario = self.request.user
+
+            if not Barbearia.objects.filter(dono=usuario).exists():
+                # TODO: Criar tela que avisa ao usuário que o usuário 
+                # deve cadastrar sua barbearia antes de cadastrar sua agenda
+
+                return redirect(reverse_lazy("barbearia:cadastrar_barbearia"))
 
         return super().get(request, *args, **kwargs)
 
--- barberease/usuarios/utils.py
+++ barberease/usuarios/utils.py
@@ -10,8 +10,10 @@ def manage_login_redirect(request):
         request.session['dono'] = True
 
         if Barbearia.objects.filter(dono=usuario).exists():
+            request.session['barbearia_cadastrada'] = True
             return reverse_lazy("barbearia:home")
         else:
+            request.session['barbearia_cadastrada'] = False
             return reverse_lazy("barbearia:cadastrar_barbearia")
     else:
         request.session['dono'] = False
